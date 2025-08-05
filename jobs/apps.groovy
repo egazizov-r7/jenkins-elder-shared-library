f904858602aa6cda23f6c0852ff55a36e66e85e1
Eldar Gazizov <150266617+egazizov-r7@users.noreply.github.com>
2025-08-05 11:23:23 +0200

    Basic pipeline

---
+++ jobs/apps.groovy
@@ -0,0 +1,33 @@
+String getSafeName(String job) {
+  return job.replaceAll('/', '-').replaceAll('\\.', '-').replaceAll('--', '-')
+}
+
+def apps = [
+    ['job_name': 'jenkins-elder-shared-library', 'repo_name': 'https://github.com/egazizov-r7/jenkins-elder-shared-library.git'],
+    ['job_name': 'go-cve-dictionary-elder', 'repo_name': 'https://github.com/egazizov-r7/go-cve-dictionary-elder.git', 'jenkinsfile_path': 'Jenkinsfile/goCveDictElder'],
+]
+
+apps.each { app ->
+    multibranchPipelineJob(getSafeName(app.job_name)) {
+        branchSources {
+            branchSource {
+                github {
+                    id("ID-" + app.job_name)
+                    repoOwner('egazizov-r7')
+                    repository(app.repo_name)
+                }
+            }
+        }
+        factory {
+            workflowBranchProjectFactory {
+              scriptPath(app.jenkinsfile_path == null ? "Jenkinsfile" : app.jenkinsfile_path)
+            }
+        }
+        orphanedItemStrategy {
+          discardOldItems {
+            numToKeep(5)
+            daysToKeep(7)
+          }
+        }
+    }
+}
---
+++ vars/ivmPipeline.groovy
@@ -0,0 +1,22 @@
+def call(Map pipelineParams) {
+    pipeline {
+        agent: any
+
+        parameters {
+            string(name: 'POD_IDLE_MINUTES', defaultValue: '0', description: 'Number of minutes pod will stay idle post build')
+        }
+
+        options {
+            ansiColor('xterm')
+            timestamps()
+        }
+
+        stages {
+            stage('Start Regression testing') {
+                steps {
+                    sh "echo Starting regression testing with parameters: ${pipelineParams}"
+                }
+            }
+        } // End of Stages
+    } // End of pipeline
+}
